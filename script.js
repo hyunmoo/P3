e7d2473fae3a2725b18508f1fb3b5832e25c4dbe
hyunmoo <dirgh1234@naver.com>
2014-05-08 22:32:14 +0900

    aa

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 	console.log("ready");	
+	
+	$('#im').click(function(){
+
 	$("#ssound")[0].load();
 	$("#ssound")[0].play();
+		
+	});
+	
+
 	//document.getElementById("ssound").play();
 	
 
