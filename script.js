cf1732b166688bbcadc65af013ccc1c763afcfd7
hyunmoo <dirgh1234@naver.com>
2014-05-08 21:40:12 +0900

    asd

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 			$("#tVal").html(a);
 			$("#cVal").html("Lose!" + count1);
 			a++;
-			$("m0").css({'-webkit-transform':'rotate('+a+'deg)'});
+			 $("#m0").rotate(a);
 		}else if(count === 1){
 			if( temp_x != Math.abs(Number(number1)) ){
 				count1++
